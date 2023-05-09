a5c12153ce83a70e997295f4dca5515b890a94ca
Anthony Church <ag.church21@gmail.com>
2023-05-09 11:27:18 +0100

    Setting up integration tests file and express plus tools.

---
+++ __tests__/app.test.js
@@ -0,0 +1,9 @@
+const request = require("supertest");
+const data = require("../db/data/test-data");
+const app = require("../app");
+
+describe("/api/topics endpoint", () => {
+    test("First test", () => {
+
+    })
+})
