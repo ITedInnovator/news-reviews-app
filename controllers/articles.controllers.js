d2d203fd1cbc0cf0616ff837a75472eb01adc274
Anthony Church <ag.church21@gmail.com>
2023-05-11 10:55:27 +0100

    Moved logic for 404 error into model for endpoint.

--- controllers/articles.controllers.js
+++ controllers/articles.controllers.js
@@ -2,13 +2,10 @@ const { selectArticle } = require("../models/articles.models");
 
 exports.getArticleById = ( req, res, next ) => {
     const { article_id } = req.params;
-    selectArticle(article_id).then( ({ rows }) => {
-        const article = rows[0];
-        if(!article){
-            return Promise.reject({status: 404, msg: 'There is not an article at this ID sorry!'});
-        } else{
-            res.status(200).send({ article });
-        }
+    selectArticle(article_id).then( (article) => {
+        
+        
+        res.status(200).send({ article });
         
     }).catch(err => {
         next(err);
--- models/articles.models.js
+++ models/articles.models.js
@@ -1,6 +1,13 @@
 const db = require("../db/connection");
 
 exports.selectArticle = (article_id) => {
+    
     const sql = `SELECT * FROM articles WHERE article_id = $1;`;
-    return db.query(sql, [article_id]);
+    return db.query(sql, [article_id]).then(({rows}) => {
+        const article = rows[0];
+
+        if(!article) return Promise.reject({status: 404, msg: 'There is not an article at this ID sorry!'});
+
+        return article;
+    })
 }
